1e0b7ac89aea28a31421aae29b95980614b06875
Skander <skanderchebil1@gmail.com>
2022-02-28 00:10:20 +0100

    initialising development environment

---
+++ src/Components/Name/Name.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./Name.scss";
+
+function Name() {
+  return (
+    <div>
+      <input type='text' />
+    </div>
+  );
+}
+
+export default Name;
