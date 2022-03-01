62e442cec37cebec014e85d0e2317c7e66f6119e
Skander <skanderchebil1@gmail.com>
2022-03-01 14:39:35 +0100

    added API support of similiar choosed color

--- src/Components/ColorPicker/ColorPicker.jsx
+++ src/Components/ColorPicker/ColorPicker.jsx
@@ -1,24 +1,55 @@
 import React, { useEffect, useRef, useState } from "react";
+import "./ColorPicker.scss";
 
 function ColorPicker() {
   const colorRef = useRef();
   const [color, setColor] = useState("");
+  const [similarColors, updateSimilarColors] = useState();
+  const numberOfSimilarColors = [0, 1, 2, 3, 4];
 
   useEffect(() => {
     //for a random color on each mount
     let randomHex = Math.floor(Math.random() * 16777215).toString(16); //operation to get a random valid hex code
     setColor(`#${randomHex}`);
   }, []);
-  // add color api here for similar colors
+
+  async function getSimilarColors() {
+    const colorVar = color;
+    let cleanHexCode = colorVar.split("");
+    cleanHexCode.shift();
+    cleanHexCode = cleanHexCode.join("");
+
+    const fetched = await fetch(
+      `https://www.thecolorapi.com/scheme?hex=${cleanHexCode}&count=5`
+    );
+    const res = await fetched.json();
+    const data = await res.colors;
+    //console.log(data[0].hex.value);
+    updateSimilarColors(data);
+  }
+
+  useEffect(() => {
+    getSimilarColors();
+    // console.log(similarColors);
+  }, [color]);
 
   return (
     <div>
       <input
+        className='colorInput'
         onChange={(e) => setColor(e.target.value)}
         value={color}
         ref={colorRef}
         type='color'
       />
+      {similarColors
+        ? numberOfSimilarColors.map((entry) => (
+            <li
+              key={entry}
+              style={{ backgroundColor: `${similarColors[entry].hex.value}` }}
+            ></li>
+          ))
+        : null}
     </div>
   );
 }
