3eac8c35c9d394e63a4cb359d72c6707e24741f5
Skander <skanderchebil1@gmail.com>
2022-03-07 00:10:58 +0100

    started on the canvas compo

--- src/Components/Canvas/Canvas.jsx
+++ src/Components/Canvas/Canvas.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Canvas.scss";
 function Canvas() {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    canvasRef.current.getContext("2d");
+    console.log(canvasRef);
+  }, []);
+
+  function startPainting(e) {
+    console.log(e.nativeEvent);
+  }
+
+  function painting() {}
+
+  function endPainting() {}
+
   return (
     <div className='canvas'>
-      <canvas></canvas>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={startPainting}
+        onMouseMove={painting}
+        onMouseUp={endPainting}
+      ></canvas>
     </div>
   );
 }
