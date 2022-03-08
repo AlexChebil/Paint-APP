0180e10f8c0aa7761e77f57dbcbde247ba7e152e
Skander <skanderchebil1@gmail.com>
2022-03-08 15:46:21 +0100

    added lineWidth/Opacity to the canvas compo as well as an eraser

--- src/Components/Canvas/Canvas.jsx
+++ src/Components/Canvas/Canvas.jsx
@@ -3,11 +3,15 @@ import { ColorContext } from "../Context/Context";
 import "./Canvas.scss";
 
 function Canvas() {
-  const [isDrawing, setIsDrawing] = useState(false);
+  const width = window.innerWidth - 20;
+  const height = window.innerHeight - 375;
   const canvasRef = useRef();
   const canvasContext = useRef();
   const { mainColor } = useContext(ColorContext);
-  const [drawingColor, setDrawingColor] = useState();
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(25);
+  const [lineOpacity, setLineOpacity] = useState(0.5);
+  const [isErasing, setIsErasing] = useState(true);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -20,7 +24,11 @@ function Canvas() {
   }, []);
 
   useEffect(() => {
-    setDrawingColor(mainColor);
+    canvasContext.current.lineWidth = lineWidth;
+    canvasContext.current.globalAlpha = lineOpacity;
+  }, [lineWidth, lineOpacity]);
+
+  useEffect(() => {
     if (mainColor) {
       canvasContext.current.strokeStyle = mainColor;
     }
@@ -46,14 +54,65 @@ function Canvas() {
     canvasContext.current.stroke();
   }
 
+  function earaseHandler() {
+    setIsErasing((prev) => !prev);
+
+    switch (true) {
+      case isErasing:
+        canvasContext.current.strokeStyle = "white";
+        break;
+
+      default:
+        canvasContext.current.strokeStyle = mainColor;
+        break;
+    }
+  }
+
   return (
     <div className='canvas'>
       <canvas
+        width={width}
+        height={height}
         ref={canvasRef}
         onMouseDown={StartPainting}
         onMouseUp={EndPainting}
         onMouseMove={Painting}
       ></canvas>
+
+      <div className='settings'>
+        <div className='lineWidth'>
+          <p>Line Width</p>
+          <input
+            type='range'
+            value={lineWidth}
+            min={1}
+            max={50}
+            onChange={(e) => setLineWidth(e.target.value)}
+          />
+        </div>
+
+        <div className='lineOpacity'>
+          <p>lineOpacity</p>
+          <input
+            type='range'
+            step='0.1'
+            min={0.1}
+            max={1}
+            value={lineOpacity}
+            onChange={(e) => setLineOpacity(e.target.value)}
+          />
+        </div>
+
+        {isErasing ? (
+          <button style={{ backgroundColor: "red" }} onClick={earaseHandler}>
+            Erase
+          </button>
+        ) : (
+          <button style={{ backgroundColor: "green" }} onClick={earaseHandler}>
+            Erasing
+          </button>
+        )}
+      </div>
     </div>
   );
 }
